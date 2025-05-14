24502fbbb4e04d8caf7a6777a6f99c61550fe1db
Marco Oegg <marco.oegg@gmail.com>
2025-05-14 22:34:09 +0000

    styles

--- html/tp_1/ejercicio_19.php
+++ html/tp_1/ejercicio_19.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 16</title>
+    <title>Ejercicio 19</title>
     <link rel="stylesheet" href="static/src/main.css">
 </head>
 <body>  
     <header>
-        <h1>Ejercicio 16</h1>
+        <h1>Ejercicio 19</h1>
     </header>
     <div id="main">
     <?php
