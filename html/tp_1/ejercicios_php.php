63acd1af91595600b9d6332665c5acce3819e184
Marco Oegg <marco.oegg@gmail.com>
2025-05-14 22:12:33 +0000

    [ADD][ejecicios_php.php]

---
+++ html/tp_1/ejercicios_php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="static/img/favicon.ico" type="image/x-icon">
+    <script src="static/src/ejercicios.js"></script>
+    <link rel="stylesheet" href="static/src/main.css">
+    <title>Ejercicios en PHP</title>
+</head>
+<body>
+    <h1>Ejercicios en PHP</h1>
+    <p>
+        <a href="ejercicio_15.php">Ejercicio 15</a><br>
+        <a href="ejercicio_16.php">Ejercicio 16</a><br>
+        <a href="ejercicio_17.php">Ejercicio 19</a><br>
+    </p>
+</body>
+</html>
