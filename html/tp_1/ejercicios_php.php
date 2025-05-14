491b77d9819f2b373a794574097165d32eb603b5
Marco Oegg <marco.oegg@gmail.com>
2025-05-14 23:47:39 +0000

    [ADD] ejercicios 2 a 14 en php link a github

--- html/tp_1/ejercicios_php.php
+++ html/tp_1/ejercicios_php.php
@@ -10,7 +10,18 @@
 </head>
 <body>
     <h1>Ejercicios en PHP</h1>
-    <p>
+    <>
+        
+    <a href="ejercicios2-5.php">Ejercicio 2 al 5</a><br>
+        <a href="ejercicio6.php">Ejercicio 6</a><br>
+        <a href="ejercicio7.php">Ejercicio 7</a><br>
+        <a href="ejercicio8.php">Ejercicio 8</a><br>
+        <a href="ejercicio9.php">Ejercicio 9</a><br>
+        <a href="ejercicio10.php">Ejercicio 10</a><br>
+        <a href="ejercicio11.php">Ejercicio 11</a><br>
+        <a href="ejercicio12.php">Ejercicio 12</a><br>
+        <a href="ejercicio13.php">Ejercicio 13</a><br>
+        <a href="ejercicio14.php">Ejercicio 14</a><br>
         <a href="ejercicio_15.php">Ejercicio 15</a><br>
         <a href="ejercicio_16.php">Ejercicio 16</a><br>
         <a href="ejercicio_19.php">Ejercicio 19</a><br>
