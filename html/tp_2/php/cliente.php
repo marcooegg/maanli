471958a6188caff9a2cabe7d8cf64cf4eb2a549a
Marco Oegg <marco.oegg@gmail.com>
2025-06-04 23:33:08 +0000

    add cliente.php

---
+++ html/tp_2/php/cliente.php
@@ -0,0 +1,19 @@
+<?php
+require_once "db.php";
+$input = file_get_contents("php://input");
+$data = json_decode($input);
+
+if (isset($data->cuit)) {
+    $cuit = $data->cuit;
+    $query = "SELECT * FROM persona WHERE nip = ?";
+    $params = [$cuit];
+    $cliente = $conn->read($query, $params);
+    if ($cliente) {
+        echo json_encode(["success" => true, "cliente" => $cliente[0]]);
+    } else {
+        echo json_encode(["success" => false]);
+    }
+} else {
+    echo json_encode(["success" => false]);
+}
+?>
