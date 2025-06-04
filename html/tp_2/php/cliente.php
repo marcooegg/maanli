650e7b3202bfbfc74d6f15e69209bffddbaf2c35
Marco Oegg <marco.oegg@gmail.com>
2025-06-04 23:46:58 +0000

    wip

--- html/tp_2/php/cliente.php
+++ html/tp_2/php/cliente.php
@@ -2,11 +2,11 @@
 require_once "db.php";
 $input = file_get_contents("php://input");
 $data = json_decode($input);
-echo $data;
+echo json_encode($data);
 if (isset($data->cuit)) {
     $cuit = $data->cuit;
-    $query = "SELECT * FROM persona WHERE nip = ?";
-    $params = [$cuit];
+    $query = "SELECT * FROM persona WHERE nip = :cuit";
+    $params = [":cuit" => $cuit];
     $cliente = $conn->read($query, $params);
     if ($cliente) {
         echo json_encode(["success" => true, "cliente" => $cliente[0]]);
@@ -14,6 +14,6 @@ if (isset($data->cuit)) {
         echo json_encode(["success" => false]);
     }
 } else {
-    echo json_encode(["success" => false]);
+    echo json_encode(["success" => false, "message" => "CUIT no proporcionado"]);
 }
 ?>
