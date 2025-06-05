d323c0d70e718a945f588113fc1865d47e050d04
marcooegg <marco.oegg@gmail.com>
2025-06-05 19:37:13 -0300

    wip

--- html/tp_2/index.php
+++ html/tp_2/index.php
@@ -197,6 +197,7 @@
                 });
         };
 
+        document.querySelector("#btnCrearFactura").addEventListener("click", crearFactura);
         agregarLinea(); // Agregar la primera línea al cargar la página
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
