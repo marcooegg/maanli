737b765a5d2ca004837dff100dec2ea3c4b920b3
marcooegg <marco.oegg@gmail.com>
2025-06-05 20:21:12 -0300

    WIP

---
+++ html/tp_2/php/productos.php
@@ -0,0 +1,22 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    require_once "db.php";
+
+    $descripcion = $_GET['descripcion'] ?? '';
+
+    try {
+        $conn = new DataBaseConnection();
+        $query = "SELECT * FROM product WHERE descripcion = :descripcion";
+        $res = $conn->read($query, [":descripcion" => $descripcion]);
+
+        echo $res
+            ? json_encode(["success" => true, "message" => $res])
+            : json_encode(["success" => false, "message" => "Credenciales inválidas"]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(["success" => false, "message" => "Error interno", "error" => $e->getMessage()]);
+    }
+    
+?>
