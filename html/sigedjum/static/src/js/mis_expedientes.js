d49ae34f2f74b880944bfdfbb650dc7ab7dcbdb3
marcooegg <marco.oegg@gmail.com>
2025-08-10 10:11:27 -0300

    [FIX] - mis_expedientes

--- html/sigedjum/static/src/js/mis_expedientes.js
+++ html/sigedjum/static/src/js/mis_expedientes.js
@@ -1,9 +1,9 @@
-import { createApp, h } from 'vue';
+const { createApp } = Vue;
 
-const app = createApp({
+createApp({
   data() {
     return {
-      expedientes: [],
+      tableData: [],
       searchQuery: '',
     };
   },
@@ -14,7 +14,7 @@ const app = createApp({
       })
       .then(res => {
         if (res.data.success) {
-          this.expedientes = res.data.expedientes;
+          this.tableData = res.data.expedientes;
         } else {
           alert('Error al cargar expedientes');
         }
@@ -26,43 +26,9 @@ const app = createApp({
     },
     goCrear() {
       window.location.href = 'crear_expediente.html';
-    },
+    }
   },
   mounted() {
     this.showTable();
-  },
-  render() {
-    if (!this.expedientes.length) {
-      return h('p', 'No hay expedientes para mostrar.');
-    }
-    return h('table', { class: 'table table-bordered' }, [
-      h('thead', [
-        h('tr', [
-          h('th', 'ID'),
-          h('th', 'Título'),
-          h('th', 'Descripción'),
-          h('th', 'Estado'),
-          h('th', 'Creado'),
-          h('th', 'Acciones'),
-        ]),
-      ]),
-      h('tbody', this.expedientes.map(exp =>
-        h('tr', { key: exp.id }, [
-          h('td', exp.id),
-          h('td', exp.title),
-          h('td', exp.description),
-          h('td', exp.status),
-          h('td', exp.created_at),
-          h('td', [
-            h('button', {
-              class: 'btn btn-info btn-sm',
-              onClick: () => this.ver(exp.id),
-            }, 'Ver'),
-          ]),
-        ])
-      )),
-    ]);
-  },
-});
-
-app.mount('#table-container');
+  }
+}).mount('#app');
