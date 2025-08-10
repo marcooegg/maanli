4734a2b8bbd362ffd41dd1cd30f76fcdac083d7a
marcooegg <marco.oegg@gmail.com>
2025-08-10 13:41:42 -0300

    [FIX] notas

--- html/sigedjum/static/src/js/ver_notas.js
+++ html/sigedjum/static/src/js/ver_notas.js
@@ -1,49 +1,49 @@
 const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-      tableData: [],
-      cargando: false,
-      error: '',
-      notas: [],
-      searchQuery: '',
-    };
-  },
-  methods: {
-    showTable() {
-        this.cargando = true;
-        const params = new URLSearchParams(window.location.search);
-      const id = params.get('id');
-      axios.get('api/list_notas.php', {
-        params: { id }
-      })
-      .then(res => {
-        if (res.data.success) {
-          this.tableData = res.data.notes;
-          this.notas = res.data.notes;
-        } else {
-          alert('Error al cargar notes');
+    data() {
+        return {
+            tableData: [],
+            cargando: false,
+            error: '',
+            notas: [],
+            searchQuery: '',
+        };
+    },
+    methods: {
+        showTable() {
+            this.cargando = true;
+            const params = new URLSearchParams(window.location.search);
+            const id = params.get('expediente_id');
+            axios.get('api/list_notas.php', {
+                params: { id }
+            })
+                .then(res => {
+                    if (res.data.success) {
+                        this.tableData = res.data.notes;
+                        this.notas = res.data.notes;
+                    } else {
+                        alert('Error al cargar notes');
+                    }
+                })
+                .catch(() => alert('Error al conectar con el servidor')).finally(() => {
+                    this.cargando = false;
+                });
+        },
+        goCrear() {
+            window.location.href = 'crear_nota.html';
+        },
+        volverExpte() {
+            const params = new URLSearchParams(window.location.search);
+            const expedienteId = params.get('id');
+            if (expedienteId) {
+                window.location.href = `ver_expediente.html?id=${expedienteId}`;
+            } else {
+                alert('No se especificó ID del expediente.');
+            }
         }
-      })
-      .catch(() => alert('Error al conectar con el servidor')).finally(() => {
-        this.cargando = false;
-      });
     },
-    goCrear() {
-      window.location.href = 'crear_nota.html';
-    },
-    volverExpte() {
-      const params = new URLSearchParams(window.location.search);
-      const expedienteId = params.get('id');
-      if (expedienteId) {
-        window.location.href = `ver_expediente.html?id=${expedienteId}`;
-      } else {
-        alert('No se especificó ID del expediente.');
-      }
+    mounted() {
+        this.showTable();
     }
-  },
-  mounted() {
-    this.showTable();
-  }
 }).mount('#app');
