0d5943514423e5ed79fba776b2cf4fbc9403b9bc
Marco Oegg <marco.oegg@gmail.com>
2025-10-23 23:06:41 +0000

    add api clientes

---
+++ api_clientes/db.php
@@ -0,0 +1,60 @@
+<?php
+    // require_once '../env/secret.php';
+    // $DB_HOST = 'c282.ferozo.com';
+    // $DB_HOST = "localhost";
+    // $DB_PORT = '2092';
+    // $DB_USER = 'c2821127_JURI_01';
+    // $DB_PASS = 'PUnuru87fi';
+    // $DB_NAME = 'c2821127_JURI_01';
+    // $DB_CHARSET = 'utf8mb4';
+    class DataBaseConnection {
+        public $pdo;
+
+        public function __construct($host = "localhost", $port = "2092", $user = "c2821127_JURI_01", $pass = "PUnuru87fi", $name = "c2821127_JURI_01", $charset = "utf8mb4") {
+            try {
+                $this->pdo = new PDO("mysql:host=$host;port=$port;dbname=$name;charset=$charset", $user, $pass);
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Error de conexión: " . $e->getMessage());
+            }
+        }
+        public function getConnection() {
+            return $this->pdo;
+        }
+
+        public function closeConnection() {
+            $this->pdo = null;
+        }
+
+        public function read($fields = ["*"], $table = "", $where="", $fetchMode = PDO::FETCH_ASSOC) {
+            $fields = implode(',', $fields);
+            $query = "SELECT {$fields} FROM {$table}";
+            if ($where) {
+                $query .= "WHERE " . $where;
+            }
+            
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll($fetchMode);
+        }
+
+        // public function write(string $table,array $fields_values, $fetchMode = PDO::FETCH_ASSOC) {
+        //     $fields = array_keys($fields_values);
+        //     $values = array_values($fields_values);
+        //     $query = "INSERT INTO {$table} ({$fields}) VALUES {$values}";
+
+        //     $stmt = $this->pdo->prepare($query);
+        //     $res = $stmt->execute($params);
+        //     return $this->pdo->lastInsertId();
+        // }
+
+        // public function call(string $method,array $fields_values){
+        //     $fields = array_keys($fields_values);
+        //     $values = array_values($fields_values);
+        //     $query = "CALL {$method}($fields)";
+        //     $stmt = $this->pdo->prepare($query);
+        //     $res = $stmt->execute($params);
+        //     return $this->pdo->lastInsertId();
+        // }
+    }
+?>
---
+++ api_clientes/insert.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once "db.php";
+
+// try {
+//     $conn = new DataBaseConnection();
+//     $pdo = $conn->getConnection();
+
+//     $stmt = $pdo->query("SELECT id_cliente, nombre, dni, telefono FROM `clientes` ORDER BY id_cliente");
+//     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//     echo json_encode(['success' => true, 'clientes' => $data]);
+// } catch (PDOException $e) {
+//     echo json_encode(['success' => false, 'error' => $e->getMessage()]);
+// }
+
+echo json_encode(['ok'=>true])
+
+?>
